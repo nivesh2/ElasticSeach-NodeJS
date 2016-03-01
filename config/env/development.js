dc8a46f39a8257a894a8b26e9658e6b4989a9a63
nivesh singh <nivesh2@gmail.com>
2016-03-01 20:15:10 +0000

    config commited

---
+++ config/env/development.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    port: 808,
+    redisPort:12169,
+    redisHostName:'pub-redis-12169.us-east-1-3.3.ec2.garantiadata.com',
+    password:'#myDatabase7',
+    elasticConnectionUrl:'localhost:9200'
+};
