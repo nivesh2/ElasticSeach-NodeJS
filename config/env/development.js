a57aea194ec82835e295e33498d5877171c4b47f
Nivesh Singh <nivesh2@gmail.com>
2016-10-04 21:49:06 +0530

    config updated

--- config/env/development.js
+++ config/env/development.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-    port: 808,
-    redisPort:12169,
-    redisHostName:'pub-redis-12169.us-east-1-3.3.ec2.garantiadata.com',
-    password:'#myDatabase7',
+    port: 8080,
+    redisPort:__Your_RedisDB_Port__,
+    redisHostName:'__remoteRedisUrl__',
+    password:'DatabasePassword',
     elasticConnectionUrl:'localhost:9200'
 };
